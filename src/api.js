97ec044f980b6ec2348c932bb22886d5b69a1b32
juliomatcom <juliomatcom@users.noreply.github.com>
2017-10-28 13:23:50 +0200

    hide api key

--- src/api.js
+++ src/api.js
@@ -1,13 +1,12 @@
-const HOST = 'https://api.etherscan.io/api'
-const API_KEY = 'TIMRV1B6YVN8TCR719MRWG68F5UW1TSSBC'
-const STATS_URL = `${HOST}?module=stats&action=ethprice&apikey=${API_KEY}`
+const {host, key} = require('../etherscanAPI')
+const STATS_URL = `${host}?module=stats&action=ethprice&apikey=${key}`
 const MIN_REQUEST_TIME = 1500
 
 function getAccounts(accounts) {
   const addresses = accounts ? JSON.parse(accounts) : []
   const addressJoined = addresses.join(',')
-  const BALANCE_URL = `${HOST}?module=account&action=balancemulti` +
-    `&tag=latest&apikey=${API_KEY}&address=${addressJoined}`
+  const BALANCE_URL = `${host}?module=account&action=balancemulti` +
+    `&tag=latest&apikey=${key}&address=${addressJoined}`
   if (addresses.length) {
     console.log(`fetch ${BALANCE_URL}`)
     return fetch(BALANCE_URL)
