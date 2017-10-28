9f74549a56e8458f864b5c1c872db42c02957a66
juliomatcom <juliomatcom@users.noreply.github.com>
2017-10-28 14:45:57 +0200

    feat qrScan btn

--- src/AddAddress.js
+++ src/AddAddress.js
@@ -3,6 +3,7 @@ import {
   AsyncStorage,
   StyleSheet,
   Image,
+  Linking,
   Text,
   TextInput,
   TouchableHighlight,
@@ -11,6 +12,7 @@ import {
 import appStyles from './lib/styles'
 import {isEthereumAddress} from './lib/utils'
 import {STG_ADDRESSES} from './lib/constants'
+const QR_APP_LINK = 'https://play.google.com/store/apps/details?id=tw.mobileapp.qrcode.banner&hl=en'
 
 export default class AddAddress extends React.Component {
   static navigationOptions = {
@@ -24,6 +26,7 @@ export default class AddAddress extends React.Component {
     super(props)
     this.navigation = this.props.navigation
     this.addAddress = this.addAddress.bind(this)
+    this.qrScanApp = this.qrScanApp.bind(this)
     this.updateText = this.updateText.bind(this)
     this.state = {
       valid: true,
@@ -59,6 +62,12 @@ export default class AddAddress extends React.Component {
     }
   }
 
+  qrScanApp() {
+    console.log('qrScan pressed')
+    Linking.openURL(QR_APP_LINK)
+      .catch(err => console.error('An error occurred', err))
+  }
+
   updateText(text){
     this.setState({
       address: text,
@@ -68,6 +77,7 @@ export default class AddAddress extends React.Component {
 
   render() {
     const submitButton = require('../assets/img/download.png')
+    const qrBtn = require('../assets/img/qr-scan.png')
     const screenProps = {
       rootNavigation: this.navigation
     }
@@ -80,7 +90,7 @@ export default class AddAddress extends React.Component {
           <View style={style.inputContainer}>
             <TextInput
               style={style.input}
-              placeholder="ethereum address"
+              placeholder="Paste your ethereum address"
              onChangeText={this.updateText}
              autoFocus={true}
              autoCorrect={false}
@@ -93,10 +103,21 @@ export default class AddAddress extends React.Component {
           {inputValidation}
         </View>
         <View style={style.containerSubmit}>
-          <TouchableHighlight key="refresh" underlayColor='transparent' onPress={this.addAddress}>
+          <TouchableHighlight underlayColor='transparent' onPress={this.addAddress}>
             <Image
-              style={style.submit}
+              style={style.img}
               source={submitButton}>
+            </Image>
+         </TouchableHighlight>
+        </View>
+        <View style={[style.center, style.scan]}>
+          <Text style={style.qrText}>
+            Or just scan the QR Code and copy the address.
+          </Text>
+          <TouchableHighlight underlayColor='transparent' onPress={this.qrScanApp}>
+            <Image
+              style={style.img}
+              source={qrBtn}>
             </Image>
          </TouchableHighlight>
         </View>
@@ -124,12 +145,19 @@ const style = StyleSheet.create({
     height: 100,
     paddingTop: 20
   },
-  submit: {
+  img: {
     width: 64,
     height: 64
   },
   validation: {
     paddingTop: 5,
     color: '#F44336' // material red 500
+  },
+  scan: {
+    paddingTop: 30
+  },
+  qrText: {
+    paddingBottom: 10,
+    fontSize: 10
   }
 })
