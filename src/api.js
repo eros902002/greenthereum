28210ed153ee97a0ec90f7d03f52c561637d4bf2
juliomatcom <juliomatcom@users.noreply.github.com>
2017-10-28 13:40:46 +0200

    refactor api.json

--- src/api.js
+++ src/api.js
@@ -1,12 +1,12 @@
-const {host, key} = require('../etherscanAPI')
-const STATS_URL = `${host}?module=stats&action=ethprice&apikey=${key}`
+const {HOST, KEY} = require('../ETHERSCAN_API.json')
+const STATS_URL = `${HOST}?module=stats&action=ethprice&apikey=${KEY}`
 const MIN_REQUEST_TIME = 1500
 
 function getAccounts(accounts) {
   const addresses = accounts ? JSON.parse(accounts) : []
   const addressJoined = addresses.join(',')
-  const BALANCE_URL = `${host}?module=account&action=balancemulti` +
-    `&tag=latest&apikey=${key}&address=${addressJoined}`
+  const BALANCE_URL = `${HOST}?module=account&action=balancemulti` +
+    `&tag=latest&apikey=${KEY}&address=${addressJoined}`
   if (addresses.length) {
     console.log(`fetch ${BALANCE_URL}`)
     return fetch(BALANCE_URL)
